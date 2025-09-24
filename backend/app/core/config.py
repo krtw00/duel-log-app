e654aa684f5f203bbebb660feaf160504e3b58fa
krtw00 <krtw00@users.noreply.github.com>
2025-09-24 22:34:00 +0900

    仮でフロントエンドとバックエンドを動作

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # .env を読み込む
+load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:password@db:5432/duellog")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql+psycopg2://postgres:password@db:5432/duellog"
+)
