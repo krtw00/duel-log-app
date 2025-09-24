5619fe4596b585372e274a964c149350ab08c77c
krtw00 <krtw00@users.noreply.github.com>
2025-09-24 22:34:00 +0900

    chore: 仮でフロントエンドとバックエンドを動作

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,18 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from core.config import DATABASE_URL
+from ..core.config import DATABASE_URL
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True,           # SQLログ出力
+    echo=True,
     future=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-# 依存関係で使用する関数
 def get_db():
     db = SessionLocal()
     try:
