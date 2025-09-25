cfb63628b602221efc5c543b7dbfe051d622df6c
H.Iguchi <grandchariot001@gmail.com>
2025-09-25 22:53:10 +0900

    DECKのCURDAPIを実装

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from .db.session import engine, Base
-from .models.models import User, Deck, Duel, SharedURL
+from .models import User, Deck, Duel, SharedURL
+from .api import decks
 
 # Alembic を使う場合はここで create_all は不要
 # Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Duel Log API")
+
+app.include_router(decks.router)
 
 @app.get("/")
 def root():
