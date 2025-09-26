c69d087986ccfa1f3358bb6638d95454c1b987c9
krtw00 <krtw00@users.noreply.github.com>
2025-09-26 22:51:43 +0900

    USERのPOSTのAPIを作成

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,1 +1,6 @@
-from .models import User, Deck, Duel, SharedURL
+from .user import User
+from .deck import Deck
+from .duel import Duel
+from .sharedURL import SharedURL
+
+__all__ = ["User", "Deck", "Duel", "SharedURL"]
