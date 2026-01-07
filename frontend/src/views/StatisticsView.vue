774cef2ab55da6d25c18ab180eaaa7e78b3e99a3
krtw00 <krtw00@users.noreply.github.com>
2026-01-07 23:36:01 +0900

    feat: colorize my デッキ win rates

--- frontend/src/views/StatisticsView.vue
+++ frontend/src/views/StatisticsView.vue
@@ -154,7 +154,13 @@
                       density="compact"
                     >
                       <template #[`item.win_rate`]="{ item }">
-                        {{ item.wins }} / {{ item.total_duels }} ({{ item.win_rate.toFixed(1) }}%)
+                        <v-chip
+                          size="small"
+                          :color="getMatchupColor(item.win_rate)"
+                          :variant="getMatchupColor(item.win_rate) ? 'tonal' : 'outlined'"
+                        >
+                          {{ item.wins }} / {{ item.total_duels }} ({{ formatPercent(item.win_rate) }})
+                        </v-chip>
                       </template>
                       <template #no-data>
                         <div class="no-data-placeholder py-8">
