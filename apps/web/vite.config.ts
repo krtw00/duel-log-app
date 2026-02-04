0d4e1ac7312fa8f421fdd82d1159e60e0b68b98d
krtw00 <krtw00@users.noreply.github.com>
2026-02-04 16:07:15 +0900

    fix: set vite root for web

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: resolve(__dirname),
   plugins: [react(), tailwindcss()],
   envDir: '../../',
   resolve: {
